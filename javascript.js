b92acf2c6a10e1de1def410c8a4b63110de08061
ttdd33 <67976010+ttdd33@users.noreply.github.com>
2026-04-07 13:39:57 +0200

    Javasritp táblázat zoom eltűnési hibájának megoldása

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ var selectedIndex = null;
 var array1 = new Array(); 
 array1.push({"Id":"1","sutinev":"Süni","sutitipus":"vegyes","dijazott":"nem"});
 
-array1.push({"Id":"2","sutinev":"Gesztenyealagut","sutitipus":"vegyes","dijazott":"igen"});
+array1.push({"Id":"2","sutinev":"Gesztenyealaguuuut","sutitipus":"vegyes","dijazott":"igen"});
 printArray();
 
 function printArray(){
