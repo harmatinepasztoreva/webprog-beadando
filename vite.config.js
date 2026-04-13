0b64c4fd1fc9c8639389be955004f3caa302aae3
ttdd33 <67976010+ttdd33@users.noreply.github.com>
2026-04-13 11:58:31 +0200

    vite.config elérési utak frissítése, és a build mappa feltöltése

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Relatív elérési utak beállítása
+  base: './', 
+  
+  build: {
+    rollupOptions: {
+      // React oldalak megadása.
+      input: {
+     
+        reactoldal: resolve(__dirname, 'react.html'),
+        spa: resolve(__dirname, 'spa.html'),
+        axios: resolve(__dirname, 'axios.html'),
+  
+      }
+    }
+  }
 })
